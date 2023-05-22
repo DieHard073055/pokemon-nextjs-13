3d70d82e67451ea167a23740a0424f3b5b7f4bb6
Diehard073055 <eshanshafeeq073055@gmail.com>
2023-05-23 05:14:57 +0800

    add PokemonTable and SSRPokemonTable

---
+++ components/SSRPokemonTable.tsx
@@ -0,0 +1,8 @@
+import { Pokemon } from "@/app/types";
+import PokemonTable from "./PokemonTable";
+import { store } from "@/store";
+
+const SSRPokemonTable = () => {
+    return <PokemonTable pokemons={store.getState().search.startupPokemon} />
+}
+export default SSRPokemonTable
